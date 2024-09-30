5ad5c2de98e39d49ff0dc444afd46b4cc7dfa6aa
LEv145 <you@example.com>
2024-09-30 10:31:44 +0200

    Docs v 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "AI assistant parsers template"
 copyright = "2024, Lev145"
 author = "Lev145"
-release = "0.1.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
