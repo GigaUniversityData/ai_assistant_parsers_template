25f9619d333eb9b482c959b4514b339034288036
LEv145 <you@example.com>
2024-09-30 10:09:54 +0200

    Теперь шаблон не использует `SimpleSelectPageBaseParser`

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/parsers/page/_template.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/parsers/page/_template.py
@@ -2,12 +2,13 @@ from bs4 import BeautifulSoup
 
 from ai_assistant_parsers_core.parsers.utils.clean_blocks import *
 from ai_assistant_parsers_core.parsers.utils.restructure_blocks import *
-from ai_assistant_parsers_core.parsers import SimpleSelectPageBaseParser
+from ai_assistant_parsers_core.parsers import SimpleSelectDomainBaseParser
 
 
-class ВСТАВИТЬ_ТЕКСТ_СЮДАPageParser(SimpleSelectPageBaseParser):
+class ВСТАВИТЬ_ТЕКСТ_СЮДАPageParser(SimpleSelectDomainBaseParser):
     def __init__(self) -> None:
         super().__init__(
-            allowed_paths=["ВСТАВИТЬ_ТЕКСТ_СЮДА"],
+            allowed_domains_paths=["ВСТАВИТЬ_ТЕКСТ_СЮДА"],
+            included_paths=["ВСТАВИТЬ_ТЕКСТ_СЮДА"],
             select_arguments=["ВСТАВИТЬ_ТЕКСТ_СЮДА"],
         )
