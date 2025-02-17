3b822aa35c7be4b9c4ea8baa2213d2dedecf417d
LEv145 <you@example.com>
2025-02-17 21:28:54 +0200

    Исправлена опечатка, из-за чего имя в `parse_one` не заменялось на нужное

--- {{cookiecutter.project_slug}}/scripts/parse_one.py
+++ {{cookiecutter.project_slug}}/scripts/parse_one.py
@@ -5,6 +5,6 @@ import shlex
 
 URL = sys.argv[1]
 PYTHON_PATH = sys.executable
-COMMAND = f"{PYTHON_PATH} -m ai_assistant_parsers_core.cli parse-one ai_assistant_parsers_spbu output/parse_one {URL}"
+COMMAND = f"{PYTHON_PATH} -m ai_assistant_parsers_core.cli parse-one {{cookiecutter.project_slug}} output/parse_one {URL}"
 
 subprocess.run(shlex.split(COMMAND))
